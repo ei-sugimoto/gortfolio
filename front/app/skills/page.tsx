4cca6e0f7723fd69f9cce2db3a8479a4ead42aac
ei-sugimoto <eikun1014@gmail.com>
2024-08-17 17:30:50 +0900

    add other's skill

--- front/app/skills/page.tsx
+++ front/app/skills/page.tsx
@@ -28,6 +28,13 @@ export default function Skills() {
             alt="ruby"
             src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ruby/ruby-original.svg"
           />
+
+          <Image
+            alt="C++"
+            width={60}
+            height={60}
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-plain.svg"
+          />
         </HStack>
         <Heading>framework</Heading>
         <HStack w={"100%"} justify={"space-evenly"}>
@@ -67,6 +74,35 @@ export default function Skills() {
           />
         </HStack>
         <Heading>Others</Heading>
+        <HStack w={"100%"} justify={"space-evenly"}>
+          <Image
+            width={60}
+            height={60}
+            alt="docker"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original-wordmark.svg"
+          />
+
+          <Image
+            width={60}
+            height={60}
+            alt="mysql"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg"
+          />
+
+          <Image
+            width={60}
+            height={60}
+            alt="postgresql"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original-wordmark.svg"
+          />
+
+          <Image
+            width={60}
+            height={60}
+            alt="prisma"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/prisma/prisma-original.svg"
+          />
+        </HStack>
       </VStack>
     </>
   );
